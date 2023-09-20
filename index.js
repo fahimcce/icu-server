c5843d483aadc98757ef2e98c9fec50c5525a4fd
Fahim334712 <f.fahimcox@gmail.com>
2023-09-21 00:59:53 +0600

    blood section add

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ async function run() {
         const doctorCollection = database.collection('doctors');
         const medicineCollection = database.collection('medicines');
         const labCollection = database.collection('labs');
+        const donarCollection = database.collection('donar');
 
         // Middleware for admin authorization
         function isAdmin(req, res, next) {
@@ -43,24 +44,19 @@ async function run() {
         app.use('/admin', isAdmin);
 
 
-
-
-
-
+        // -------------------------------ICU_Part-------------------------------
         // Get ICU (Accessible to all users)
         app.get('/icu', async (req, res) => {
             const cursor = icuCollection.find();
             const result = await cursor.toArray();
             res.send(result);
         });
-
         // Post ICU (Admin-only access)
         app.post('/icu', async (req, res) => {
             const icu = req.body;
             const result = await icuCollection.insertOne(icu);
             res.send(result);
         });
-
         // Delete single icu (Admin-only access)
         app.delete('/icu/:id', async (req, res) => {
             const id = req.params.id;
@@ -68,45 +64,52 @@ async function run() {
             const result = await icuCollection.deleteOne(query);
             res.send(result);
         });
+        // ----------------------------------------------------------------------------
 
-        // Get Doctors (Accessible to all users)
+
+        // ------------------------------------Doctors_Part----------------------------
+        // Get Doctors 
         app.get('/doctors', async (req, res) => {
             const cursor = doctorCollection.find();
             const result = await cursor.toArray();
             res.send(result);
         });
-
-        // Add new doctor (Admin-only access)
+        // Add new doctor 
         app.post('/doctors', async (req, res) => {
             const doctors = req.body;
             const result = await doctorCollection.insertOne(doctors);
             res.send(result);
         });
+        // ----------------------------------------------------------------------------
 
+
+
+
+        // -----------------------------medicine_Parts-----------------------------
         //get medicine
         app.get('/medicine', async (req, res) => {
             const cursor = medicineCollection.find();
             const result = await cursor.toArray();
             res.send(result);
         });
-
-        //delete medicine
-        app.delete('/medicine/:id', async (req, res) => {
-            const id = req.params.id;
-            console.log("delete id", id)
-            const query = { _id: new ObjectId(id) }
-            const result = await medicineCollection.deleteOne(query)
-            res.send(result);
-        });
-
         // Post medicines
         app.post('/medicine', async (req, res) => {
             const medicine = req.body;
             const result = await medicineCollection.insertOne(medicine);
             res.send(result);
         });
+        //delete medicine
+        app.delete('/medicine/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await medicineCollection.deleteOne(query)
+            res.send(result);
+        });
+        // --------------------------------------------------------------------
 
 
+
+        //-----------------------------Diagnostic Part-----------------------------
         // get lab Test
         app.get('/lab', async (req, res) => {
             const cursor = labCollection.find();
@@ -117,6 +120,28 @@ async function run() {
         app.post('/lab', async (req, res) => {
             const lab = req.body;
             const result = await labCollection.insertOne(lab);
+            res.send(result);
+        });
+        // Delete lab test
+        app.delete('/lab/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await labCollection.deleteOne(query)
+            res.send(result);
+        });
+        // -----------------------------------------------------------------------------
+
+        // ---------------------------------BloodDonar----------------------------------
+        // get donars
+        app.get('/donar', async (req, res) => {
+            const cursor = donarCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+        // post donars
+        app.post('/donar', async (req, res) => {
+            const donar = req.body;
+            const result = await donarCollection.insertOne(donar);
             res.send(result);
         });
 
